f3995f102552e17e6cc8ada8dff1a44a4783c4ff
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-04-17 14:27:43 +0200

    add ProfileFooter

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import ProfileContent from "./ProfileContent";
+import ProfileFooter from "./ProfileFooter";
 
 const Profile = () => {
   return (
@@ -10,6 +11,7 @@ const Profile = () => {
         </Col>
         <Col md={4}></Col>
       </Row>
+      <ProfileFooter />
     </Container>
   );
 };
