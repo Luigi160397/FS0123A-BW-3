487bbfae67e5053a518f914e782403a21663387a
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-04-17 17:14:48 +0200

    add Card Profile (analisi e risorse)

--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -1,11 +1,13 @@
 import AnalisiCard from "./AnalisiCard";
 import MainCard from "./MainCard";
+import RisorseCard from "./RisorseCard";
 
 const ProfileContent = () => {
   return (
     <div>
       <MainCard />
       <AnalisiCard/>
+      <RisorseCard/>
     </div>
   );
 };
