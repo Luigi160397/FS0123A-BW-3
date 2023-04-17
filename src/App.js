5659fc44b164c060cbfd2adb43e277993b857ca1
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-04-17 17:34:36 +0200

    added some changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import MyNav from "./components/MyNav";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MyNav />
+        <Sidebar />
+        <Routes>
+
+        </Routes>
+
       </BrowserRouter>
     </div>
   );
