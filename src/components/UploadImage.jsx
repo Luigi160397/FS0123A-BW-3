426e0ad4ad63cfb1768d5f174aa91dd020beffea
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-18 16:35:18 +0200

    test

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -3,57 +3,59 @@ import { Button, Form, Modal } from "react-bootstrap";
 import { getProfileAction } from "../redux/actions";
 import { useDispatch } from "react-redux";
 
-const UploadImage = ({ show, handleClose, profile}) => {
+const UploadImage = ({ show, handleClose, profile }) => {
+  const [image, setImage] = useState(null);
+  const dispatch = useDispatch();
+  const id = profile._id;
 
-    const [image, setImage] = useState();
+  const uploadImg = async e => {
+    e.preventDefault();
+    const token = process.env.REACT_APP_API_KEY;
 
-    const dispatch=useDispatch();
+    try {
+      if (!image) {
+        alert("Please select an image to upload");
+        return;
+      }
 
-    const id=profile._id;
+      const formData = new FormData();
+      formData.append("profile", image);
+      formData.append("username", profile.username);
 
-    const uploadImg = async (e) => {
-        e.preventDefault();
-        const token = process.env.REACT_APP_API_KEY;
-        try {
-          const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${id}/picture`, {
-            method: "POST",
-            body: image,
-            headers: {
-              "Content-Type": image.type,
-              Authorization: token
-            }
-          });
-          if (response.ok) {
-            dispatch(getProfileAction());
-            handleClose();
-          }
-        } catch (error) {
-          alert(error);
+      const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${id}/picture`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: token
         }
-      };
+      });
 
-    return (
-        <Modal show={show} onHide={handleClose} className="text-white">
-            <Modal.Header className="bg-dark text-white" closeButton>
-                <Modal.Title>Aggiungi immagine</Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="bg-dark">
-                <Form onSubmit={uploadImg}>
-                    <Form.Group className="mb-3" controlId="upload">
-                        <Form.Label>Carica immagine</Form.Label>
-                        <Form.Control
-                            type="file"
-                            autoFocus
-                            required
-                            onChange={e => setImage(e.target.files[0])}
-                        />
-                        <Button className="mt-3" variant="primary" type="submit">
-                            Carica
-                        </Button>
-                    </Form.Group>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    );
-}
+      if (response.ok) {
+        dispatch(getProfileAction());
+        handleClose();
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose} className="text-white">
+      <Modal.Header className="bg-dark text-white" closeButton>
+        <Modal.Title>Aggiungi immagine</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="bg-dark">
+        <Form onSubmit={uploadImg}>
+          <Form.Group className="mb-3" controlId="upload">
+            <Form.Label>Carica immagine</Form.Label>
+            <Form.Control type="file" autoFocus required onChange={e => setImage(e.target.files[0])} />
+            <Button className="mt-3" variant="primary" type="submit">
+              Carica
+            </Button>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
 export default UploadImage;
