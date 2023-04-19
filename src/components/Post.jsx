895f88db5b12ccdb3f8b7d81b54f890a5268d156
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-04-19 22:20:40 +0200

    creazione componente EditPost

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import { useDispatch, useSelector } from "react-redux";
 import { getPostsAction } from "../redux/actions";
 import avatar1 from "../assets/avatar1.png";
 import { FaPen } from "react-icons/fa";
+import EditPost from "./EditPost";
 
 const Post = () => {
 
-  const profile = useSelector(state => state.profile.content);
+  const [selectedPost, setSelectedPost] = useState(null);
 
+  const profile = useSelector(state => state.profile.content);
   const dispatch = useDispatch();
+
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleCloseEdit = () => setShowEdit(false);
+  const handleShowEdit = () => setShowEdit(true);
 
   useEffect(() => {
     dispatch(getPostsAction());
@@ -23,14 +30,26 @@ const Post = () => {
 
   return (
     <>
+      {selectedPost && (
+        <>
+          <EditPost selectedPost={selectedPost} showEdit={showEdit} handleCloseEdit={handleCloseEdit} />
+        </>
+      )}
+
       {posts.slice(0, 20).map(post => (
         <Card className="bg-dark text-white mt-3 position-relative" key={post._id}>
           {posts.length > 0 && post && post.user && (
             <CardHeader className="d-flex gap-2">
-              { profile && post.user._id && post.user._id === profile._id &&
+              {profile && post.user._id && post.user._id === profile._id &&
                 <Button
                   style={{ right: "10px", top: "10px" }}
-                  variant="outline-secondary border-0 py-1 px-2 rounded-circle position-absolute">
+                  variant="outline-secondary border-0 py-1 px-2 rounded-circle position-absolute"
+                  onClick={() => {
+                    console.log("Post:"+post);
+                    setSelectedPost(post);
+                    handleShowEdit();
+                  }}
+                >
                   <FaPen className="text-white fs-5" />
                 </Button>
               }
