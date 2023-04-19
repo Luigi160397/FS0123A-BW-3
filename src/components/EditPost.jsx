2e4fea57b692b4b221bcd0a5a1a37d8225116130
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-04-19 23:52:41 +0200

    implementazione EditPost

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,155 +1,123 @@
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostsAction } from "../redux/actions";
 
-const EditPost = ({ showEdit, handleCloseEdit, post }) => {
+const EditPost = ({ showEdit, handleCloseEdit, selectedPost }) => {
 
-    /*const idUser = post.user;
+    const idPost = selectedPost._id;
+    const profile = useSelector(state => state.profile.content);
     const dispatch = useDispatch();
-    const [post, setPost] = useState(null);
-  
+    const [post, setPost] = useState({});
+
     useEffect(() => {
-      setPost({
-        _id: post._id,
-        text: post.text 
-      });
-      console.log(esperienza, experience);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [esperienza]);
-  
-    const editExp = async e => {
-      e.preventDefault();
-  
-      const token = process.env.REACT_APP_API_KEY;
-      try {
-        const response = await fetch(
-          `https://striveschool-api.herokuapp.com/api/profile/${idUser}/experiences/${esperienza._id}`,
-          {
-            method: "PUT",
-            body: JSON.stringify(experience),
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: token
+        setPost({
+            _id: selectedPost._id,
+            text: selectedPost.text,
+            image: selectedPost.image
+        });
+        console.log(selectedPost, post);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedPost]);
+
+    const editPost = async e => {
+        e.preventDefault();
+
+        const token = process.env.REACT_APP_API_KEY;
+        try {
+            const response = await fetch(
+                `https://striveschool-api.herokuapp.com/api/posts/${idPost}`,
+                {
+                    method: "PUT",
+                    body: JSON.stringify(post),
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: token
+                    }
+                }
+            );
+            if (response.ok) {
+                dispatch(getPostsAction());
+                handleCloseEdit();
             }
-          }
-        );
-        if (response.ok) {
-          dispatch(getExperiencesAction(idUser));
-          handleCloseEdit();
+        } catch (error) {
+            alert(error);
         }
-      } catch (error) {
-        alert(error);
-      }
     };
-  
+
     const handleDelete = async () => {
-      const token = process.env.REACT_APP_API_KEY;
-      try {
-        const response = await fetch(
-          `https://striveschool-api.herokuapp.com/api/profile/${idUser}/experiences/${esperienza._id}`,
-          {
-            method: "DELETE",
-            headers: {
-              Authorization: token
+        const token = process.env.REACT_APP_API_KEY;
+        try {
+            const response = await fetch(
+                `https://striveschool-api.herokuapp.com/api/posts/${idPost}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: token
+                    }
+                }
+            );
+            if (response.ok) {
+                dispatch(getPostsAction());
+                handleCloseEdit();
             }
-          }
-        );
-        if (response.ok) {
-          dispatch(getExperiencesAction(idUser));
-          handleCloseEdit();
+        } catch (error) {
+            alert(error);
         }
-      } catch (error) {
-        alert(error);
-      }
     };
-    // console.log(esperienza);
+
     return (
-      <Modal show={showEdit} onHide={handleCloseEdit} className="text-white">
-        <Modal.Header className="bg-dark text-white" closeButton>
-          <Modal.Title>Aggiungi esperienza</Modal.Title>
-        </Modal.Header>
-        {experience && (
-          <Modal.Body className="bg-dark">
-            <Form onSubmit={editExp}>
-              <Form.Group className="mb-3" controlId="role">
-                <Form.Label>Qualifica</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Es: Sales Manager"
-                  autoFocus
-                  required
-                  value={experience.role}
-                  onChange={e => setExperience({ ...experience, role: e.target.value })}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="company">
-                <Form.Label>Nome Azienda</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Es: Salini srl"
-                  autoFocus
-                  required
-                  value={experience.company}
-                  onChange={e => setExperience({ ...experience, company: e.target.value })}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="area">
-                <Form.Label>Località</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Es: Roma"
-                  autoFocus
-                  required
-                  value={experience.area}
-                  onChange={e => setExperience({ ...experience, area: e.target.value })}
-                />
-              </Form.Group>
-              <div className="d-flex gap-3">
-                <Form.Group className="mb-3" style={{ flexBasis: "50%" }} controlId="startDate">
-                  <Form.Label>Data inizio</Form.Label>
-                  <Form.Control
-                    type="date"
-                    autoFocus
-                    required
-                    value={experience.startDate}
-                    onChange={e => setExperience({ ...experience, startDate: e.target.value })}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" style={{ flexBasis: "50%" }} controlId="endDate">
-                  <Form.Label>Data fine</Form.Label>
-                  <Form.Control
-                    type="date"
-                    autoFocus
-                    required
-                    value={experience.endDate}
-                    onChange={e => setExperience({ ...experience, endDate: e.target.value })}
-                  />
-                </Form.Group>
-              </div>
-              <Form.Group className="mb-3" controlId="description">
-                <Form.Label>Descrizione</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  placeholder="Descrivi la tua esperienza lavorativa"
-                  rows={3}
-                  required
-                  value={experience.description}
-                  onChange={e => setExperience({ ...experience, description: e.target.value })}
-                />
-              </Form.Group>
-              <Button className="me-3" variant="primary" type="submit">
-                Salva
-              </Button>
-              <Button variant="danger" type="button" onClick={handleDelete}>
-                Elimina
-              </Button>
-            </Form>
-          </Modal.Body>
-        )}
-      </Modal>
-    );*/
-    return(
-        <h1>Edit post</h1>
-    )
-  };
+        <Modal show={showEdit} onHide={handleCloseEdit} className="text-white">
+            <Modal.Header className="bg-dark text-white" closeButton>
+                <Modal.Title>Modifica un post</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="bg-dark">
+                {post && post.text && (
+                    <div className="d-flex gap-2 align-items-center mb-2">
+                        <img
+                            className="border border-dark border-2 rounded-circle "
+                            src={profile.image}
+                            alt="avatar"
+                            width={48}
+                            height={48}
+                        />
+                        <p>
+                            {profile.name} {profile.surname}
+                        </p>
+                    </div>
+                )}
+                <Form onSubmit={editPost}>
+                    <Form.Group className="mb-3" controlId="description">
+                        <Form.Control
+                            as="textarea"
+                            className="bg-dark border-0 p-0 text-white"
+                            placeholder="Di che cosa vuoi parlare?"
+                            rows={3}
+                            required
+                            value={post.text}
+                            onChange={e => setPost({ ...post, text: e.target.value })}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="description">
+                        <Form.Control
+                            type="text"
+                            className="bg-dark border-0 p-0 text-white"
+                            placeholder="Inserisci un url di un immagine"
+                            value={post.image}
+                            onChange={e => setPost({ ...post, image: e.target.value })}
+                        />
+                    </Form.Group>
+                    <Button className="rounded-pill" variant="secondary" type="submit">
+                        Pubblica
+                    </Button>
+                    <Button className="rounded-pill" variant="danger" type="button" onClick={handleDelete}>
+                        Elimina
+                    </Button>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+};
 
 export default EditPost;
+
